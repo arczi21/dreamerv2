f10862d91a27369e5f5d601af7cd0a0b60405598
arczi21 <tomaszarczewski2@gmail.com>
2023-06-21 18:42:54 +0200

    features updated

--- dreamerv2/plugen/features.py
+++ dreamerv2/plugen/features.py
@@ -14,11 +14,14 @@ def get_features(data):
     ram = data['ram'].numpy().astype(int)
     x = ram[:, :, 99]
     y = ram[:, :, 101]
-    z = ram[:, :, 77]
     left = (x <= 128)
     down = (y >= 128)
-    count = (z >= 40)
-    features = np.stack([left, down, count], axis=2)
+    score0 = ram[:, :, 80].astype(int)
+    score1 = ram[:, :, 82].astype(int)
+    score2 = ram[:, :, 84].astype(int)
+    score = (score0 / 5) * 100 + (score1 / 5) * 10 + score2 / 5
+    score = (score >= 126.5)
+    features = np.stack([left, down, score], axis=2)
     return features.astype(int)
 
 
